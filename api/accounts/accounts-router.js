ecbbbf5e03bb11179fc8392947a536ad1c5d861b
naushinhoque <fnmh@yahoo.com>
2023-12-07 15:58:38 -0600

    w

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -32,9 +32,10 @@ router.put('/:id', checkAccountId, checkAccountNameUnique, checkAccountPayload, 
   }
 });
 
-router.delete('/:id', checkAccountId, (req, res, next) => {
+router.delete('/:id', checkAccountId, async (req, res, next) => {
   try {
-    res.json('delete account')
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
   } catch (err) {
     next(err)
   }
